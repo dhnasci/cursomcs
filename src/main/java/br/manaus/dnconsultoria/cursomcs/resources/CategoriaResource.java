edbcb92c66abaa560fe8bb1ad9b9fd9e441d44fc
Dirceu Henrique <dhnasci@hotmail.com>
2020-01-30 15:40:45 -0400

    Testando a primeira classe de dominio: Categoria

--- src/main/java/br/manaus/dnconsultoria/cursomcs/resources/CategoriaResource.java
+++ src/main/java/br/manaus/dnconsultoria/cursomcs/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package br.manaus.dnconsultoria.cursomcs.resources;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.manaus.dnconsultoria.cursomcs.domain.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public ArrayList<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		ArrayList<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
