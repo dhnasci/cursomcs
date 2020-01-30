69d2c0d909a6fb0feee5f0e73f630bd3642922e6
Dirceu Henrique <dhnasci@hotmail.com>
2020-01-30 10:40:16 -0400

    Projeto criado para persistencia de modelagem conceitual no Sprint

---
+++ src/main/java/br/manaus/dnconsultoria/cursomcs/CursomcsApplication.java
@@ -0,0 +1,13 @@
+package br.manaus.dnconsultoria.cursomcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/manaus/dnconsultoria/cursomcs/CursomcsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.manaus.dnconsultoria.cursomcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
