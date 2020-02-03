1127db92cc972036741c38cdc4ad6e8c6cea1c38
Dirceu Henrique <dhnasci@hotmail.com>
2020-02-03 18:28:45 -0400

    Criando operação de instanciação

--- src/main/java/br/manaus/dnconsultoria/cursomcs/CursomcsApplication.java
+++ src/main/java/br/manaus/dnconsultoria/cursomcs/CursomcsApplication.java
@@ -1,13 +1,32 @@
 package br.manaus.dnconsultoria.cursomcs;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.manaus.dnconsultoria.cursomcs.domain.Categoria;
+import br.manaus.dnconsultoria.cursomcs.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcsApplication {
+public class CursomcsApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
